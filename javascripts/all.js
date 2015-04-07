dded77db1e75efaae047c35abc434cb83e8f48e3
pierretucoulat <pierretucoulat@gmail.com>
2015-04-07 15:03:38 +0200

    Automated commit at 2015-04-07 13:03:38 UTC by middleman-deploy 1.0.0

--- javascripts/all.js
+++ javascripts/all.js
@@ -12104,6 +12104,57 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 
 
 
+$(document).ready(function() {
+  // navbar transition jQuery script
+  $(window).scroll(function(e){
+    if ($(this).scrollTop() > 750) {
+      $(".navbar").css({
+        "background": "white",
+        "box-shadow": "0 0 2px white"
+      });
+    }
+    else {
+      $(".navbar").css({
+        "background": "transparent",
+        "box-shadow": "0 0 0px transparent"
+      });
+    }
+  });
+});
+
+// $(document).ready(function() {
+//   // navbar transition jQuery script
+//   $(window).scroll(function(e){
+//     if ($(this).scrollTop() > 200) {
+//       $(".navbar").slideUp();
+//     }
+//     else {
+//       $(".navbar").slideDown();
+//     }
+//   });
+// });
+$(document).ready(function() {
+  $("#banner-bg-home").css("height", $(window).height());
+});
+
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
 
 
 
